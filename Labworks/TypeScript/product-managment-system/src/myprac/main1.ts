d5d445bd1341aa6ff367f9ad0398c5b5af3f8b25
Bhargav <asc04bhargav@gmail.com>
2024-10-18 12:29:22 +0530

    ts assesment completed

--- Labworks/TypeScript/product-managment-system/src/myprac/main1.ts
+++ Labworks/TypeScript/product-managment-system/src/myprac/main1.ts
@@ -6,6 +6,7 @@ console.log("Welcome to registration form.");
 const formManager=new FormManager();
 
 const form1:formm={
+    id:1,
     name: "Bhargav",
     age: 22,
     gender: "Male",
@@ -14,8 +15,32 @@ const form1:formm={
     address: "Rompicherla, chittoor,AP."
 }
 
+const form2:formm={
+    id: 2,
+    name: "Mani",
+    age: 22,
+    gender: "Male",
+    mobileNumber: 815367282,
+    maritalStatus: "UnMarried",
+    address: "Rompicherla"
+}
+
+const form3:formm={
+    id: 3,
+    name: "Sagar",
+    age: 21,
+    gender: "Male",
+    mobileNumber: 875453,
+    maritalStatus: "UnMarried",
+    address: "Rompicherla"
+}
+
 formManager.addData(form1);
+formManager.addData(form2);
+formManager.addData(form3);
 
 let form:formm[]=formManager.listData();
 
 console.log(form);
+
+formManager.searchData(2);
