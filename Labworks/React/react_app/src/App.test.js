22c1686c6f427044e7fa623fafd14511d3a5503e
bhargav <asc04bhargav@gmail.com>
2024-12-10 17:46:25 +0530

    uploading postman

--- Labworks/React/react_app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from 'react-dom';
-import App from "./app";
-
-it('panders without craching', ()=>{
-    const div = document.createElement('div');
-    ReactDom.render(<App></App>,div);
-})
--- Labworks/React/react_app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-// import '../src/index.css';
-import './index.css';
-import App from "./app";
-
-
-ReactDom.render(<App/>, document.getElementById('root'));
